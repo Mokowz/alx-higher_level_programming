b93340a96b7f5abde80051f8679a67de093841de
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-03 01:13:36 -0800

    Check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a cycle exists
+ * @list: Linked list
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *notMove;
+
+	temp = list;
+	notMove = list;
+
+	while (temp)
+	{
+		if (temp->next == NULL)
+			return (0);
+		temp = temp->next;
+		if (temp->next == notMove)
+			return (1);
+	}
+
+	return (0);
+}
