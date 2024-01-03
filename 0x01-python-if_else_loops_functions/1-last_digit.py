031daa1dbaa7407fc8a44f935ec878825e93bc2d
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-03 01:37:50 -0800

    Get last digit of negative too

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_digit = 0
 if number >= 0:
     last_digit = number % 10
 else:
-    last_digit = 10 - (number % 10)
+    last_digit = -10 + (number % 10)
 
 if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
