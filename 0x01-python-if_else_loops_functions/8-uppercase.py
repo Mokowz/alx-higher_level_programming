156f61e167489e3e02b12565a01234b10bcced96
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-03 00:13:03 -0800

    Return uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    str1 = ''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            str1 += chr(ord(c) - 32)
+        else:
+            str1 += c
+
+    return str1
