6a9cfe93c34dd3ed11c06a30716a2dae9e27643f
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-07 10:16:45 -0800

    Find the biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max_int = 0
+    if len(max_int) == 0:
+        return None
+    for num in my_list:
+        if num > max_int:
+            max_int = num
+
+    return max_int
