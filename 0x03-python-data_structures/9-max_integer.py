2aebeb210137753df41ee82f3c03595c5ca7ee3b
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-08 12:34:33 -0800

    Fix error on file 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 def max_integer(my_list=[]):
     max_int = 0
-    if len(max_int) == 0:
+    if len(my_list) == 0:
         return None
     for i, num in enumerate(my_list):
         if i == 0 or num > max_int:
