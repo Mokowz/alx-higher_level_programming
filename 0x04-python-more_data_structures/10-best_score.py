196e544dd6ff15f943906e7a53a1df39e0da0fd9
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-10 09:39:49 -0800

    Return key of largest

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        score = None
+        key = None
+        for k in a_dictionary.keys():
+            if score is None or a_dictionary[k] > score:
+                score = a_dictionary[k]
+                key = k
+
+        return key
