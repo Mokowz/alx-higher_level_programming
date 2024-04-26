3a24ff6d825ac01d17cd626b6e97b7b4d5881a69
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-04-26 17:35:06 +0300

    Try to get the problem

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,15 +4,17 @@
 import sys
 import requests
 
-url = "https://api.github.com/repos/{}/{}/commits".format(
-    sys.argv[2], sys.argv[1])
 
-response = requests.get(url)
-commits = response.json()
-try:
-    for i in range(10):
-        print("{}: {}".format(
-            commits[i].get("sha"),
-            commits[i].get("commit").get("author").get("name")))
-except IndexError:
-    pass
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,16 @@ import sys
 import requests
 
 
-letter = "" if len(sys.argv) == 1 else sys.argv[1]
-payload = {"q": letter}
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-try:
-    response = req.json()
-    if response == {}:
-        print("No result")
-    else:
-        print("[{}] {}".format(response.get("id"), response.get("name")))
-except ValueError:
-    print("Not a valid JSON")
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
