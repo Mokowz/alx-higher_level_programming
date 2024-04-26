8a814a64a7850204e5df2180e5ae9e782a6b8a79
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-04-26 06:55:48 -0700

    Add new lines to the scripts

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a GET request to the URL, and displays the body of the response
+# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -s "$1" -H "X-School-User-Id: 98"
